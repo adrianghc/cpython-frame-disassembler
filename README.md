0234c94db465bab74b8743d5d383775af805e6ea
Adrian Georg Herrmann <adrian.ghc@outlook.com>
2025-08-05 13:36:00 +0200

    Initial commit
    This initial commit contains:
    
    - frame_disassembler.c C code file
    - frame_disassembler.pyi Python stub file
    - pyproject.toml to build wheel
    - requirements.txt for pip
    - Barebones WIP README
    - .gitignore

---
+++ README.md
@@ -0,0 +1,9 @@
+# CPython Frame Disassembler
+
+## Prerequisites
+
+`sudo apt install python3-dev`
+
+## Install
+
+`python -m build`
---
+++ frame_disassembler/frame_disassembler.pyi
@@ -0,0 +1,7 @@
+def disassemble_frame(*, marker: str = "MARKER") -> None:
+    """
+    Disassemble the current Python frame using C.
+    The `marker` argument is used to identify the disassembled frame in the
+    output.
+    """
+    ...
