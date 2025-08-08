f7ca0773c69871eedbf82cd526b69d5c14b6ffa0
Adrian Georg Herrmann <adrian.ghc@outlook.com>
2025-08-08 16:11:15 +0200

    Small formatting fixes
    Remove trailing whitespaces, use /* */ comment style for sections.

--- frame_disassembler/frame_disassembler.cpp
+++ frame_disassembler/frame_disassembler.cpp
@@ -1,9 +1,9 @@
 #include "Python.h"
 #include <cstdio>
 
-// Minimal Shiboken compatibility layer
+/* Minimal Shiboken compatibility layer */
 namespace Shiboken {
-    
+
     // Simple GIL State manager
     class GilState {
         PyGILState_STATE m_state;
@@ -28,76 +28,75 @@ namespace Shiboken {
             }
 #endif
         }
-        
+
         ~GilState() { 
             if (m_valid) {
                 PyGILState_Release(m_state);
             }
         }
-        
+
         bool isValid() const { return m_valid; }
-        
+
         // Disable copy
         GilState(const GilState&) = delete;
         GilState& operator=(const GilState&) = delete;
     };
-    
+
     class AutoDecRef {
         PyObject* m_ptr;
     public:
         explicit AutoDecRef(PyObject* p = nullptr) : m_ptr(p) {}
         ~AutoDecRef() { Py_XDECREF(m_ptr); }
-        
+
         void reset(PyObject* p = nullptr) {
             Py_XDECREF(m_ptr);
             m_ptr = p;
         }
-        
+
         PyObject* object() const { return m_ptr; }
         operator PyObject*() const { return m_ptr; }
-        
+
         // Disable copy
         AutoDecRef(const AutoDecRef&) = delete;
         AutoDecRef& operator=(const AutoDecRef&) = delete;
     };
 }
 
-// This useful function is for debugging
 static void disassembleFrame(const char *marker)
 {
     using Shiboken::AutoDecRef;
     Shiboken::GilState gil;  // Ensure we have the GIL
-    
+
     // Check if we're running on no-GIL Python
     if (!gil.isValid()) {
         fprintf(stderr, "\n%s ERROR: frame_disassembler does not support free-threaded Python (3.13t).\n", marker);
         fprintf(stderr, "%s       VSCode debugging is unreliable with --disable-gil.\n\n", marker);
         return;
     }
-    
+
     PyObject *error_type, *error_value, *error_traceback;
     PyErr_Fetch(&error_type, &error_value, &error_traceback);
-    
+
     static PyObject *dismodule = PyImport_ImportModule("dis");
     if (!dismodule) {
         PyErr_Restore(error_type, error_value, error_traceback);
         return;
     }
-    
+
     static PyObject *disco = PyObject_GetAttrString(dismodule, "disco");
     if (!disco) {
         PyErr_Restore(error_type, error_value, error_traceback);
         return;
     }
-    
+
     static PyObject *const _f_lasti = PyUnicode_FromString("f_lasti");
     static PyObject *const _f_lineno = PyUnicode_FromString("f_lineno");
     static PyObject *const _f_code = PyUnicode_FromString("f_code");
     static PyObject *const _co_filename = PyUnicode_FromString("co_filename");
-    
+
     AutoDecRef ignore{};
     auto *frame = reinterpret_cast<PyObject *>(PyEval_GetFrame());
-    
+
     if (frame == nullptr) {
         fprintf(stdout, "\n%s BEGIN no frame END\n\n", marker);
     } else {
@@ -105,18 +104,18 @@ static void disassembleFrame(const char *marker)
         AutoDecRef f_lineno(PyObject_GetAttr(frame, _f_lineno));
         AutoDecRef f_code(PyObject_GetAttr(frame, _f_code));
         AutoDecRef co_filename(PyObject_GetAttr(f_code, _co_filename));
-        
+
         long line = PyLong_AsLong(f_lineno);
         const char *fname = PyUnicode_AsUTF8(co_filename);
-        
+
         fprintf(stdout, "\n%s BEGIN line=%ld %s\n", marker, line, fname);
         ignore.reset(PyObject_CallFunctionObjArgs(disco, f_code.object(), f_lasti.object(), nullptr));
         fprintf(stdout, "%s END line=%ld %s\n\n", marker, line, fname);
     }
-    
+
     static PyObject *stdout_file = PySys_GetObject("stdout");
     ignore.reset(PyObject_CallMethod(stdout_file, "flush", nullptr));
-    
+
     PyErr_Restore(error_type, error_value, error_traceback);
 }
 
