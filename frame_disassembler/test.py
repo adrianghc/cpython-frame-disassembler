5954814dab712957e34b8b812b160eb244c3f8b9
Adrian Georg Herrmann <adrian.ghc@outlook.com>
2025-08-08 11:12:43 +0200

    Move test.py to frame_disassembler directory
    Keep all code files in one place, and avoid ModuleNotFoundError caused
    by name conflict between module name and source directory name.

---
+++ frame_disassembler/test.py
@@ -0,0 +1,3 @@
+import frame_disassembler
+
+frame_disassembler.disassemble_frame(marker="MY_MARKER")
--- test.py
+++
@@ -1,4 +0,0 @@
-import frame_disassembler.frame_disassembler as frame_disassembler
-# import frame_disassembler
-
-frame_disassembler.disassemble_frame(marker="MY_MARKER")
